6d5e6c878e810bbf82ec05f639db88aef8f1927c
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2025-10-27 14:38:53 +0530

    login.php changed

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -1,12 +1,13 @@
 <?php 
     require_once 'assets/incld/header.php';
-?>
-    <div>
-        <form>
-            <input type="text"      class="form-control"    name="mail_id"  value="bhupesh.k@aiopcpl.in">
-            <input type="password"  class="form-control"    name="pass_wd"  value="P@55word">
-            <input type="submit"    class="btn btn-primary" name="submit"   value="login">
-        </form>
+?>  <div class="container">
+        <div class="row">
+            <form>
+                <input type="text"      class="form-control"    name="mail_id"  value="bhupesh.k@aiopcpl.in">
+                <input type="password"  class="form-control"    name="pass_wd"  value="P@55word">
+                <input type="submit"    class="btn btn-primary" name="submit"   value="login">
+            </form>
+        </div>
     </div>
 <?php
     require_once 'assets/incld/jslib.php';
