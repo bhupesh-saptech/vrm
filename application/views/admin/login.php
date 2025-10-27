010ee9f7519da0049b1c0767ee24d4947e88bb30
Bhupesh Kumbhar <bhupesh.saptech@gmail.com>
2025-10-27 14:23:32 +0530

    initial commit

---
+++ application/views/admin/login.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Welcome to Vendor Relationship Managment</title>
+    </head>
+    <body>
+        <form>
+            <input type="text"      name="mail_id"  value="bhupesh.k@aiopcpl.in">
+            <input type="password"  name="pass_wd"  value="P@55word">
+            <input type="submit"    name="submit"   value="login">
+        </form>
+    </body>
+</html>
